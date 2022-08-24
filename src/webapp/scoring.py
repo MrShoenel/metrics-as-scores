7580ead5052ebca0c19427cc72a6a34d4e850a86
Sebastian Hönel <development@hoenel.net>
2022-08-24 21:22:07 +0200

    Completely overhaul webapp; make use of pregenerated CDFs, etc.

--- src/webapp/scoring.py
+++ src/webapp/scoring.py
@@ -12,15 +12,10 @@ in your browser.
 
 '''
 import os
+import ptvsd
 from sys import path
 
 path.append(os.getcwd())
-
-import ptvsd
-from src.data.metrics import MetricID
-
-
-
 
 if os.environ['BOKEH_VS_DEBUG'] == 'true':
     # 5678 is the default attach port in the VS Code debug configurations
@@ -34,12 +29,18 @@ if os.environ['BOKEH_VS_DEBUG'] == 'true':
 
 import pandas as pd
 import numpy as np
-
+from typing import Union
+from src.data.metrics import MetricID
+from collections import OrderedDict
+from pickle import load
 from bokeh.events import MenuItemClick
 from bokeh.io import curdoc
 from bokeh.layouts import column, row
-from bokeh.models import ColumnDataSource, Dropdown
+from bokeh.models import ColumnDataSource, Dropdown, CheckboxGroup, Button
+from bokeh.palettes import Category20_12
 from bokeh.plotting import figure
+from bokeh.models.widgets.markups import Div
+from src.distribution.distribution import ECDF, KDECDF_approx
 
 
 def cap(s: str) -> str:
@@ -52,89 +53,204 @@ systems_domains_df = pd.read_csv('./files/systems-domains.csv')
 systems_domains = dict(zip(systems_domains_df.System, systems_domains_df.Domain))
 systems_qc_names = dict(zip(systems_domains_df.System, systems_domains_df.System_QC_name))
 temp = systems_domains_df.groupby('Domain').count()
-domains = dict(zip(temp.index.to_list(), temp['System'].to_list()))
-dd_domain_items = [(f'[All system types] ({temp.System.sum()})', '__ALL__')] + list([(cap(f'{item[0]} ({item[1]})'), item[0]) for item in domains.items()])
-selected_domain = dd_domain_items[0]
-dd_domain = Dropdown(label=selected_domain[0], menu=dd_domain_items)
+domains = OrderedDict(zip(temp.index.to_list(), temp['System'].to_list()))
+domains_labels = OrderedDict({ key: f'{cap(key)} ({val})' for (key, val) in domains.items() })
+domains['__ALL__'] = int(temp.System.sum())
+domains_labels['__ALL__'] = '[All domain types combined]'
 
 
 
 # Data and UI for the metric/score types
 dd_scores_items = list([(f'[{item.name}] {item.value}', item.name) for item in list(MetricID)])
-selected_score = dd_scores_items[0]
+selected_score = dd_scores_items[13] # LCOM
 dd_scores = Dropdown(label=selected_score[0], menu=dd_scores_items)
+
+
+# Data and UI for cutting off distributions
+cbg_cutoff_items = ['Cut off smoothed distributions beyond actual values']
+selected_cutoff = False
+cbg_cutoff = CheckboxGroup(labels=cbg_cutoff_items, active=[])
+
+
+
+# Data and UI for selected CDF type
+dd_denstype_items = [('PDF from KDE', 'PDF'), ('ECDF', 'ECDF'), ('Smoothed approx. ECDF from KDE', 'KDE_CDF_approx'), ('[Score] ECCDF', 'ECCDF'), ('[Score] Smoothed approx. ECCDF from KDE', 'KDE_CCDF_approx')]
+selected_denstype = dd_denstype_items[4]
+dd_denstype = Dropdown(label=selected_denstype[0], menu=dd_denstype_items)
+
+
+# Contain button
+btn_contain = Button(label='Contain plot')
 
 
 
 
 # Set up data
-source = ColumnDataSource(data=dict(x=[0], y=[0]))
+#source = ColumnDataSource(data=dict(x=[0], y=[0]))
+source = ColumnDataSource(data=pd.DataFrame(columns=list([f'x_{domain}' for domain in domains.keys()]) + list(domains.keys())))
 # Set up plot
-plot = figure(height=400, width=400, title="my sine wave",
-              tools="crosshair", x_range=[0, 1], y_range=[0, 1])
-
-plot.line('x', 'y', source=source, line_width=3, line_alpha=0.6)
-
-
-from src.distribution.distribution import Distribution, ECDF, KDECDF_approx
-d: Distribution = None
+plot = figure(sizing_mode = 'stretch_width', height=850, title="Metrics as Scores",
+              x_axis_label='Metric Value', y_axis_label='Corresponding Score',
+              tools="crosshair,hover,pan,wheel_zoom,xwheel_zoom,ywheel_zoom,reset", x_range=[0, 1], y_range=[0 - .02, 1.02], active_scroll='wheel_zoom')
+#plot.toolbar.active_scroll = plot.select_one('wheel_zoom') #'wheel_zoom'
 
 
-def update_plot():
-    global d, domains, selected_score, selected_domain, systems_domains, systems_qc_names
-    if d is None:
-        d = Distribution(df=pd.read_csv('csv/metrics.csv'))
+for idx, domain in enumerate(domains.keys()):
+    plot.line(f'x_{domain}', domain, source=source, line_width=2, line_alpha=1., color=Category20_12[idx], legend_label=domains_labels[domain])
+
+plot.legend.location = 'top_right'
+plot.legend.click_policy = 'hide'
+
+
+
+
+
+cdfs_ECDF: dict[str, ECDF] = None
+cdfs_KDECDF_approx: dict[str, KDECDF_approx] = None
+
+try:
+    with open('./results/cdfs_ECDF.pickle', 'rb') as f:
+        cdfs_ECDF = load(f)
+    with open('./results/cdfs_KDECDF_approx.pickle', 'rb') as f:
+        cdfs_KDECDF_approx = load(f)
+except Exception as e:
+    raise Exception('This webapp relies on precomputed results. Please generate them using the file src/data/pregenerate.py before running this webapp.') from e
+
+
+
+def update_plot(contain_plot: bool=False):
+    sd = selected_denstype[1]
+    is_ecdf = 'ECDF' in sd or 'ECCDF' in sd
+    is_ccdf = 'CCDF' in sd
+    is_pdf = 'PDF' in sd
+
+    # The E(C)CDF is always cut off.
+    cbg_cutoff.disabled = is_ecdf
+
+    densities = {}
+    df_cols = { }
+    lb, ub = 0, 0
     
-    metric_id = MetricID[selected_score[1]]
-    systems = None
-    if selected_domain[1] != '__ALL__':
-        # Gather all systems with the selected domain.
-        temp = filter(lambda di: di[1] == selected_domain[1], systems_domains.items())
-        # Map the names to the Qualitas compiled corpus:
-        systems = list(map(lambda di: systems_qc_names[di[0]], temp))
+    use_densities = cdfs_ECDF if is_ecdf else cdfs_KDECDF_approx
 
-    data = d.get_cdf_data(metric_id=metric_id, unique_vals=False, systems=systems)
-    cdf = KDECDF_approx(data=data)
-    #cdf = ECDF(data=data)
-    pr = cdf.practical_range
+    for domain in domains.keys():
+        density: Union[ECDF, KDECDF_approx] = use_densities[f'{domain}_{selected_score[1]}']
+        densities[domain] = density
 
-    # Generate the new curve
-    x = np.linspace(pr[0], pr[1], int(1e3))
-    y = 1. - cdf(x)
+        pr = density.practical_range
 
-    ext = pr[1] - pr[0]
-    plot.x_range.start = pr[0] - ext * 1e-2
-    plot.x_range.end = pr[1] + ext * 1e-2
+        pr = densities[domain].practical_range
+        if pr[0] < lb:
+            lb = pr[0]
+        if pr[1] > ub:
+            ub = pr[1]
 
-    #plot.x_range = Range1d(start=pr[0], end=pr[1]) # Range() #[pr[0], pr[1]]
-    source.data = dict(x=x, y=y)
+    if not is_ecdf and selected_cutoff:
+        # This will cut off values falsely indicated by the smoothness.
+        lb = min(map(lambda _dens: _dens._range_data[0], densities.values()))
+        #if is_pdf:
+        #    lb = min(map(lambda _dens: _dens._range_data[0], densities.values()))
+        #else:
+        #    lb = min(map(lambda _dens: _dens.range[0], densities.values()))
+
+    if contain_plot:
+        ext = ub - lb
+        plot.x_range.start = lb - ext * 1e-2
+        plot.x_range.end = ub + ext * 1e-2
+
+        if is_pdf:
+            plot.y_range.end = max(map(lambda _dens: _dens.practical_range_pdf[1], densities.values()))
+            plot.y_range.start = 0. - .02 * plot.y_range.end
+        else:
+            plot.y_range.start = 0. - .02
+            plot.y_range.end = 1. + .02
+        return
+
+
+    for domain in domains.keys():
+        density = densities[domain]
+        #ext_domain = density.practical_range[1] - density.practical_range[0]
+        #lb_domain = density.practical_range[0]# - ext_domain * 0.03
+        lb_domain = density._range_data[0] if not is_ecdf and selected_cutoff else density.practical_range[0]
+        #ub_domain = density.practical_range[1]# + ext_domain * 0.03
+        ub_domain = density._range_data[1] if not is_ecdf and selected_cutoff else density.practical_range[1]
+        domain_x = np.linspace(lb_domain, ub_domain, 250 if is_pdf else 500) # PDF is slower
+        df_cols[f'x_{domain}'] = domain_x
+        if is_pdf:
+            df_cols[domain] = densities[domain].pdf(domain_x)
+        else:
+            df_cols[domain] = densities[domain].cdf(domain_x)
+            if is_ccdf:
+                df_cols[domain] = 1. - df_cols[domain]
+
+    source.data = pd.DataFrame(df_cols)
 
 
 
-def dd_system_click(evt: MenuItemClick):
-    global selected_domain
-    temp = { key: val for (val, key) in dd_domain_items }
-    dd_domain.label = temp[evt.item]
-    selected_domain = (temp[evt.item], evt.item)
+
+def get_score_type(val: str) -> str:
+    if 'PDF' in val:
+        return 'PDF'
+    if 'CCDF' in val:
+        return 'CCDF'
+    return 'CDF'
+
+
+def cbg_cutoff_click(active: list[int]):
+    global selected_cutoff
+    selected_cutoff = len(active) > 0
     update_plot()
+
+
+def dd_denstype_click(evt: MenuItemClick):
+    global selected_denstype
+    type_before = get_score_type(selected_denstype[1])
+    type_after = get_score_type(evt.item)
+    temp = { key: val for (val, key) in dd_denstype_items }
+    dd_denstype.label = temp[evt.item]
+    selected_denstype = (temp[evt.item], evt.item)
+    update_plot()
+    if type_before != type_after:
+        update_plot(contain_plot=True)
 
 
 def dd_scores_click(evt: MenuItemClick):
     global selected_score
+    score_before = selected_score[1]
+    score_after = evt.item
     temp = { key: val for (val, key) in dd_scores_items }
     dd_scores.label = temp[evt.item]
     selected_score = (temp[evt.item], evt.item)
     update_plot()
+    if score_before != score_after:
+        update_plot(contain_plot=True)
 
 
-dd_domain.on_click(dd_system_click)
+def btn_contain_click(*args):
+    update_plot(contain_plot=True)
+
+
+def read_text(file: str) -> str:
+    with open(file=file, mode='r') as f:
+        return f.read().strip()
+
+cbg_cutoff.on_click(cbg_cutoff_click)
 dd_scores.on_click(dd_scores_click)
+dd_denstype.on_click(dd_denstype_click)
+btn_contain.on_click(btn_contain_click)
 
 
+header = Div(text=read_text('./src/webapp/header.html'))
+footer = Div(text=read_text('./src/webapp/footer.html'))
 
 
-# Set up layouts and add to document
-inputs = column(dd_domain, dd_scores)
+input_row = row([dd_scores, dd_denstype])
+input_row2 = row([btn_contain, cbg_cutoff])
+plot_row = row([plot], sizing_mode='stretch_both')
+root_col = column(header, input_row, input_row2, plot_row, footer, sizing_mode='stretch_both')
 
-curdoc().add_root(row(inputs, plot, width=800))
+curdoc().add_root(root_col)
 curdoc().title = "Metrics as Scores"
+
+update_plot()
+update_plot(contain_plot=True)
