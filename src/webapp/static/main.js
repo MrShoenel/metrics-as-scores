d2d45b87a6b407ee8c175c0b84f1f77d509e9b97
Sebastian Hönel <development@hoenel.net>
2022-08-28 11:40:10 +0200

    Hide help by default, click to show

--- src/webapp/static/main.js
+++ src/webapp/static/main.js
@@ -14,4 +14,9 @@
 	authors.addEventListener('click', (elem, ev) => {
 		window.alert('This application belongs to a paper currently under double-blind review. The authors cannot be revealed until after this process.')
 	});
+
+
+	$('a[href="#help"], h3#help').click(_ => {
+		$('h3#help + div').slideDown();
+	});
 })()
