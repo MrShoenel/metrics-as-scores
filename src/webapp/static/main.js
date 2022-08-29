e3474fc1c0eb891bc0a29edb5d325374806029ef
Sebastian Hönel <development@hoenel.net>
2022-08-29 09:04:43 +0200

    Update CSS and JS for webapp

--- src/webapp/static/main.js
+++ src/webapp/static/main.js
@@ -2,21 +2,14 @@
 	const id = document.getElementById.bind(document)
 	, sleep = ms => new Promise((res, rej) => {
 		setTimeout(res, ms)
-	})
-	, asyncElem = async elemId => {
+	}), asyncElem = async elemId => {
 		while (id(elemId) === null) {
 			await sleep(10);
 		}
 		return id(elemId);
-	}
-	, authors = await asyncElem('authors');
+	}, authors = await asyncElem('authors');
 
 	authors.addEventListener('click', (elem, ev) => {
 		window.alert('This application belongs to a paper currently under double-blind review. The authors cannot be revealed until after this process.')
 	});
-
-
-	$('a[href="#help"], h3#help').click(_ => {
-		$('h3#help + div').slideDown();
-	});
-})()
+})();
