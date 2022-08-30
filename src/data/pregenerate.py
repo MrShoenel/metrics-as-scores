9d36c4d4b6c69c7835b65e559c4d03289902e7a0
Sebastian Hönel <development@hoenel.net>
2022-08-30 20:14:30 +0200

    Implement pregenerating fitted parametric distributions

--- src/data/pregenerate.py
+++ src/data/pregenerate.py
@@ -9,13 +9,13 @@ from joblib import Parallel, delayed
 from scipy.integrate import quad, quadrature
 from scipy.optimize import direct
 from src.data.metrics import MetricID
-from src.distribution.distribution import DensityFunc, DistTransform, Distribution, ECDF, KDECDF_approx
+from src.distribution.distribution import DensityFunc, DistTransform, Distribution, ECDF, KDECDF_approx, ParametricCDF
 from sklearn.model_selection import ParameterGrid
 
 
 
 
-def generate_densities(distr: Distribution, CDF: type[DensityFunc]=ECDF, unique_vals: bool=None, resample_samples=250_000, dist_transform: DistTransform=DistTransform.NONE) -> dict[str, DensityFunc]:
+def generate_densities(distr: Distribution, dens_fun: type[DensityFunc]=ECDF, unique_vals: bool=None, resample_samples=250_000, dist_transform: DistTransform=DistTransform.NONE) -> dict[str, DensityFunc]:
     metrics_ideal_df = pd.read_csv('./files/metrics-ideal.csv')
     metrics_ideal_df.replace({ np.nan: None }, inplace=True)
     metrics_ideal = { x: y for (x, y) in zip(map(lambda m: MetricID[m], metrics_ideal_df.Metric), metrics_ideal_df.Ideal) }
@@ -43,24 +43,34 @@ def generate_densities(distr: Distribution, CDF: type[DensityFunc]=ECDF, unique_
             systems = list(map(lambda di: systems_qc_names[di[0]], temp))
         
         # Only use unique values if explicitly requested or CDF-type is ECDF!
-        uvals = True if unique_vals else (CDF == ECDF)
+        uvals = True if unique_vals else (dens_fun == ECDF)
         data = distr.get_cdf_data(metric_id=metric_id, unique_vals=uvals, systems=systems)
 
+        if dens_fun == ParametricCDF:
+            try:
+                df = Distribution.fit_parametric(data=data, max_samples=20_000, metric_id=metric_id, domain=domain, dist_transform=dist_transform)
+                return (f'{domain}_{row.metric}', df)
+            except Exception as e:
+                print(f'Cannot fit parametric distribution for domain={domain}, metric={metric_id.value}')
+                return (f'{domain}_{row.metric}', None)
 
-        return (f'{domain}_{row.metric}', CDF(data=data, resample_samples=resample_samples, compute_ranges=True, ideal_value=metrics_ideal[metric_id], dist_transform=dist_transform, transform_value=transform_value, metric_id=metric_id, domain=domain))
+        # Do transformation manually for other types of DensityFunc
+        transform_value, data = Distribution.transform(data=data, dist_transform=dist_transform)
+
+        return (f'{domain}_{row.metric}', dens_fun(data=data, resample_samples=resample_samples, compute_ranges=True, ideal_value=metrics_ideal[metric_id], dist_transform=dist_transform, transform_value=transform_value, metric_id=metric_id, domain=domain))
 
     cdfs = Parallel(n_jobs=-1)(delayed(get_cdf)(i) for i in range(len(expanded_grid.index)))
     return dict(cdfs)
 
 
 if __name__ == '__main__':
-    clazzes = [ECDF, KDECDF_approx]
+    clazzes = [ParametricCDF] # [ECDF, KDECDF_approx, ParametricCDF]
     transfs = list(DistTransform)
 
     d = Distribution(df=pd.read_csv('csv/metrics.csv'))    
     for transf in transfs:
         for clazz in clazzes:
-            temp = generate_densities(distr=d, CDF=clazz, unique_vals=True, resample_samples=75_000, dist_transform=transf)
+            temp = generate_densities(distr=d, dens_fun=clazz, unique_vals=True, resample_samples=75_000, dist_transform=transf)
             with open(f'./results/cdfs_{clazz.__name__}_{transf.name}.pickle', 'wb') as f:
                 dump(temp, f)
             print(f'Finished generating CDFs for {clazz.__name__} with transform {transf.name}.')
