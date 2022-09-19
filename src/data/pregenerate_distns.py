14b25fc94d5e1952b1576b38350f503fef35921f
Sebastian Hönel <development@hoenel.net>
2022-09-19 13:54:26 +0200

    Add proper treatment for when data + RV are both discrete (i.e., integral)

--- src/data/pregenerate_distns.py
+++ src/data/pregenerate_distns.py
@@ -1,8 +1,10 @@
 from os import getcwd, cpu_count
 from sys import path
+from typing import Any
 path.append(getcwd())
 
 
+import numpy as np
 import pandas as pd
 from pickle import dump
 from nptyping import Float, NDArray, Shape
@@ -12,14 +14,16 @@ from numpy.random import default_rng
 from pickle import dump
 from joblib import Parallel, delayed
 from src.data.metrics import MetricID
-from src.distribution.distribution import DensityFunc, DistTransform, Distribution
+from src.distribution.distribution import DistTransform, Distribution, ParametricCDF, ParametricCDF_discrete
 from src.data.pregenerate_fit import fit, get_data_tuple, Continuous_RVs_dict, Discrete_RVs_dict
 from sklearn.model_selection import ParameterGrid
 
 
+metrics_discrete_df = pd.read_csv('./files/metrics-discrete.csv')
+metrics_discrete = { x: y for (x, y) in zip(map(lambda m: MetricID[m], metrics_discrete_df.Metric), metrics_discrete_df.Discrete) }
 
 
-def generate_parametric_fits(data_dict: dict[str, NDArray[Shape["*"], Float]], dist_transform: DistTransform, transform_values_dict: dict[str, float]) -> dict[str, DensityFunc]:
+def generate_parametric_fits(dist_transform: DistTransform, data_dict: dict[str, NDArray[Shape["*"], Float]], transform_values_dict: dict[str, float], data_discrete_dict: dict[str, NDArray[Shape["*"], Float]], transform_values_discrete_dict: dict[str, float]) -> list[dict[str, Any]]:
     # The thinking is this: To each data series we can always fit a continuous distribution,
     # whether it's discrete or continuous data. The same is not true the other way round, i.e.,
     # we should not fit a discrete distribution if the data is known to be continuous.
@@ -30,14 +34,12 @@ def generate_parametric_fits(data_dict: dict[str, NDArray[Shape["*"], Float]], d
     #
     # That means that for discrete data, we will have to kinds of fitted RVs.
     # Also, when fitting a continuous RV to discrete data, we will add jitter to the data.
-     
-    metrics_discrete_df = pd.read_csv('./files/metrics-discrete.csv')
-    metrics_discrete = { x: y for (x, y) in zip(map(lambda m: MetricID[m], metrics_discrete_df.Metric), metrics_discrete_df.Discrete) }
-    domains = Distribution.domains()
 
-    from scipy.stats._continuous_distns import norminvgauss_gen, genhyperbolic_gen, geninvgauss_gen, invgauss_gen, studentized_range_gen
+    domains = Distribution.domains(include_all_domain=True)
+
+    from scipy.stats._continuous_distns import norminvgauss_gen, gausshyper_gen, genhyperbolic_gen, geninvgauss_gen, invgauss_gen, studentized_range_gen
     from scipy.stats._discrete_distns import nhypergeom_gen, hypergeom_gen
-    ignored_dists = list([rv.__name__ for rv in [norminvgauss_gen, genhyperbolic_gen, geninvgauss_gen, invgauss_gen, studentized_range_gen, nhypergeom_gen, hypergeom_gen]])
+    ignored_dists = list([rv.__name__ for rv in [norminvgauss_gen, gausshyper_gen, genhyperbolic_gen, geninvgauss_gen, invgauss_gen, studentized_range_gen, nhypergeom_gen, hypergeom_gen]])
 
     param_grid = { 'domain': domains, 'metric': list(map(lambda m: m.name, MetricID)), 'rv': list([x for x in Continuous_RVs_dict.keys() if x not in ignored_dists]), 'type': ['continuous'], 'dist_transform': [dist_transform] }
     expanded_grid = pd.DataFrame(ParameterGrid(param_grid=param_grid))
@@ -48,6 +50,10 @@ def generate_parametric_fits(data_dict: dict[str, NDArray[Shape["*"], Float]], d
         row = expanded_grid.iloc[grid_idx,]
         key = f'{row.domain}_{row.metric}'
         key_u = f'{key}_u'
+        discrete_discrete = MetricID[row.metric] in metrics_discrete.keys() and row.rv in Discrete_RVs_dict.keys()
+        if discrete_discrete:
+            # Fit a discrete RV to discrete data
+            return (data_discrete_dict[key], data_discrete_dict[key_u], transform_values_discrete_dict[key])
         return (data_dict[key], data_dict[key_u], transform_values_dict[key])
 
     rng = default_rng(seed=76543210)
@@ -59,20 +65,37 @@ def generate_parametric_fits(data_dict: dict[str, NDArray[Shape["*"], Float]], d
 
 
 if __name__ == '__main__':
-    from tqdm import tqdm
+    #df = pd.read_csv('./results/temp.csv')
+    #bla = fits_to_mas_cdfs(df=df, dist_transform=DistTransform.EXPECTATION, use_continuous=False)
+    #with open('./results/cdfs_ParametricCDF_discrete_EXPECTATION.pickle', 'wb') as f:
+    #    dump(bla, f)
+    #with open('./results/distns_NONE.pickle', 'rb') as f:
+    #    from pickle import load
+    #    bla = load(f)
+    #    print(bla)
+
     print('Reading data file...')
     dist = Distribution(df=pd.read_csv('csv/metrics.csv'))
 
-
     for dist_transform in list(DistTransform):
-        collect()
         print(f'Parallel pre-processing of datasets for transform {dist_transform.name} ({dist_transform.value})...')
-        res = Parallel(n_jobs=min(len(MetricID), cpu_count()))(delayed(get_data_tuple)(dist, m, dist_transform) for m in tqdm(list(MetricID)))
+
+        use_metrics = list([m for m in MetricID if metrics_discrete[m]])
+        res = Parallel(n_jobs=min(len(use_metrics), cpu_count()))(delayed(get_data_tuple)(dist, m, dist_transform, False) for m in tqdm(use_metrics))
+        data_discrete_dict: dict[str, NDArray[Shape["*"], Float]] = dict([(item[0], item[1]) for sublist in res for item in sublist])
+        transform_values_discrete_dict: dict[str, float] = dict([(item[0], item[2]) for sublist in res for item in sublist])
+        del res
+        collect()
+        print(f'Having {len(list(data_discrete_dict.keys()))} pre-processed datasets (discrete).')
+        
+        use_metrics = list(MetricID)
+        res = Parallel(n_jobs=min(len(use_metrics), cpu_count()))(delayed(get_data_tuple)(dist, m, dist_transform, True) for m in tqdm(use_metrics))
         data_dict: dict[str, NDArray[Shape["*"], Float]] = dict([(item[0], item[1]) for sublist in res for item in sublist])
         transform_values_dict: dict[str, float] = dict([(item[0], item[2]) for sublist in res for item in sublist])
         del res
-        print(f'Having {len(list(data_dict.keys()))} pre-processed datasets.')
+        collect()
+        print(f'Having {len(list(data_dict.keys()))} pre-processed datasets (continuous).')
 
-        result = generate_parametric_fits(data_dict=data_dict, dist_transform=dist_transform, transform_values_dict=transform_values_dict)
+        result = generate_parametric_fits(dist_transform=dist_transform, data_dict=data_dict, transform_values_dict=transform_values_dict, data_discrete_dict=data_discrete_dict, transform_values_discrete_dict=transform_values_discrete_dict)
         with open(file=f'./results/pregnerate_distns_{dist_transform.name}.pickle', mode='wb') as f:
             dump(result, f)
