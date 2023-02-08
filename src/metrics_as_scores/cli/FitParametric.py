55caf0da8d13bb33122bf29cdd8814684c255e26
Sebastian Hönel <development@hoenel.net>
2023-02-08 10:42:54 +0100

    Finalize workflow for fitting parametric distributions

--- src/metrics_as_scores/cli/FitParametric.py
+++ src/metrics_as_scores/cli/FitParametric.py
@@ -28,12 +28,16 @@ class FitParametricWorkflow(Workflow):
     def __init__(self) -> None:
         super().__init__()
         self.use_ds: LocalDataset = None
+        self.ds: Dataset = None
         self.df: pd.DataFrame = None
         self.fits_dir: Path = None
+        self.selected_rvs_c: list[type[rv_continuous]] = None
+        self.selected_rvs_d: list[type[rv_discrete]] = None
         self.use_fitter: type[Fitter] = FitterPymoo
+        self.num_cpus = 1
     
 
-    def _select_continuous_rvs(self) -> Iterable[rv_continuous]:
+    def _select_continuous_rvs(self) -> Iterable[type[rv_continuous]]:
         self.q.print('''
 You can now select the continuous random variables that you want to
 attempt to fit to the data. Select all in case you intend to re-
@@ -41,9 +45,10 @@ distribute and publicize your dataset. It is often not worth to de-
 select distributions unless you have a specific reason to do so,
 because fitting a continuous distribution is comparatively cheap and
 fast.'''.strip())
-        return self.q.checkbox(message='Select continuous random variables:', choices=[Choice(title=type(rv).__name__, value=rv, checked=True) for rv in Continuous_RVs]).ask()
+        recommend_ignore = [norminvgauss_gen, gausshyper_gen, genhyperbolic_gen, geninvgauss_gen, invgauss_gen, studentized_range_gen]
+        return self.q.checkbox(message='Select continuous random variables:', choices=[Choice(title=type(rv).__name__, value=type(rv), checked=not type(rv) in recommend_ignore) for rv in Continuous_RVs]).ask()
     
-    def _select_discrete_rvs(self) -> Iterable[rv_discrete]:
+    def _select_discrete_rvs(self) -> Iterable[type[rv_discrete]]:
         self.q.print('''
 You can now select the discrete random variables that you want to
 additionally attempt to fit to discrete quantity types. Select all
@@ -51,37 +56,93 @@ in case you intend to redistribute and publicize your dataset.
 While there are much fewer discrete random variables, their fitting
 is dramatically more computationally expensive to compute, since a
 global search has to be performed.'''.strip())
-        return self.q.checkbox(message='Select discrete random variables:', choices=[Choice(title=dp[0], value=dp[1], checked=True) for dp in Discrete_Problems.items()]).ask()
+        # Note how we will use the RV's type, not the problem's!
+        from scipy.stats import _discrete_distns
+        recommended_ignore = list(rv.__name__ for rv in [nhypergeom_gen, hypergeom_gen])
+        return self.q.checkbox(message='Select discrete random variables:', choices=[Choice(title=dp[0], value=getattr(_discrete_distns, dp[0]), checked=not dp[0] in recommended_ignore) for dp in Discrete_Problems.items()]).ask()
     
 
-    def _iterable_fits(self, rvs: Iterable[Union[rv_continuous, rv_discrete]]) -> Iterable[tuple[Path, str, str, Union[rv_continuous, rv_discrete]]]:
-        from itertools import product
-        for comb in product(self.use_ds['qtypes'].keys(), self.use_ds['contexts'], rvs):
-            fit_file = f'{comb[0]}_{comb[1]}_{"c" if issubclass(type(comb[2]), rv_continuous) else "d"}_{type(comb[2]).__name__}.json'
-            fit_path = self.fits_dir.joinpath(f'./{fit_file}')
-            if fit_path.exists():
-                self.print_info(text_normal='Fit already exists, skipping: ', text_vital=fit_file)
-            else:
-                yield (fit_path, comb[0], comb[1], comb[2])
+    # def _iterable_fits(self, rvs: Iterable[Union[rv_continuous, rv_discrete]]) -> Iterable[tuple[Path, str, str, Union[rv_continuous, rv_discrete]]]:
+    #     from itertools import product
+    #     for comb in product(self.use_ds['qtypes'].keys(), self.use_ds['contexts'], rvs):
+    #         fit_file = f'{comb[0]}_{comb[1]}_{"c" if issubclass(type(comb[2]), rv_continuous) else "d"}_{type(comb[2]).__name__}.json'
+    #         fit_path = self.fits_dir.joinpath(f'./{fit_file}')
+    #         if fit_path.exists():
+    #             self.print_info(text_normal='Fit already exists, skipping: ', text_vital=fit_file)
+    #         else:
+    #             yield (fit_path, comb[0], comb[1], comb[2])
     
 
-    def _fit_continuous_rvs(self, rvs: Iterable[rv_continuous]) -> None:
-        """
-        For each quantity type, and for each context, fit each random variable
-        once, and save the result in "`qtype_context_rv`". If that file exists,
-        skip to next.
-        """
-        perms = list(self._iterable_fits(rvs=rvs))
-        with Progress() as progress:
-            task1 = progress.add_task('[cyan]Fitting continuous distributions...', total=len(perms))
-            for fit_path, qtype, ctx, rv in perms:
-                data = self.df[(self.df[self.use_ds['colname_type']] == qtype) & (self.df[self.use_ds['colname_context']] == ctx)][self.use_ds['colname_data']].to_numpy()
-                fitter = self.use_fitter(dist=type(rv))
-                res = fitter.fit(data=data)
-                with open(file=str(fit_path.resolve()), mode='w', encoding='utf-8') as fp:
-                    dump(obj=res, fp=fp)
-                progress.update(task_id=task1, advance=1)
+    # def _fit_continuous_rvs(self, rvs: Iterable[rv_continuous]) -> None:
+    #     """
+    #     For each quantity type, and for each context, fit each random variable
+    #     once, and save the result in "`qtype_context_rv`". If that file exists,
+    #     skip to next.
+    #     """
+    #     perms = list(self._iterable_fits(rvs=rvs))
+    #     with Progress() as progress:
+    #         task1 = progress.add_task('[cyan]Fitting continuous distributions...', total=len(perms))
+    #         for fit_path, qtype, ctx, rv in perms:
+    #             data = self.df[(self.df[self.use_ds['colname_type']] == qtype) & (self.df[self.use_ds['colname_context']] == ctx)][self.use_ds['colname_data']].to_numpy()
+    #             fitter = self.use_fitter(dist=type(rv))
+    #             res = fitter.fit(data=data)
+    #             with open(file=str(fit_path.resolve()), mode='w', encoding='utf-8') as fp:
+    #                 dump(obj=res, fp=fp)
+    #             progress.update(task_id=task1, advance=1)
     
+
+    def _get_data_tuples(self, dist_transform: DistTransform, continuous: bool) -> tuple[dict[str, float], dict[str, NDArray[Shape["*"], Float]]]:
+        """
+        Prepares all required datasets for one distribution transform in parallel,
+        either for continuous or discrete data.
+
+        dist_transform: ``DistTransform``
+            The chosen distribution transform.
+        
+        continuous: ``bool``
+            Passed forward to :meth:`get_data_tuple()`:
+            whether the transform is real-valued or must be converted to integer.
+
+        :rtype: ``tuple[dict[str, float], dict[str, NDArray[Shape["*"], Float]]]``
+            Returns two dictionaries.
+        
+        :return: Two dictionaries, where the keys are the same in either. The values
+            in the first are computed ideal values for the selected transform. The
+            values in the later are 1-D arrays of the data (the distances).
+        """
+        res = Parallel(n_jobs=self.num_cpus)(delayed(get_data_tuple)(ds=self.ds, qtype=qtype, dist_transform=dist_transform, continuous_transform=continuous) for qtype in tqdm(self.ds.quantity_types))
+        data_dict = dict([(item[0], item[1]) for sublist in res for item in sublist])
+        transform_values_dict = dict([(item[0], item[2]) for sublist in res for item in sublist])
+        return (transform_values_dict, data_dict)
+
+
+    def _fit_parametric(self, dist_transform: DistTransform) -> list[dict[str, Any]]:
+
+        # There are two steps to this:
+        # 1) Get all data required
+        # 2) Fit
+        # Continuous:
+        s = 'Performing distribution transforms for: '
+        self.print_info(text_normal=s, text_vital='continuous')
+        transform_values_dict, data_dict = self._get_data_tuples(dist_transform=dist_transform, continuous=True)
+        # Discrete:
+        self.print_info(text_normal=s, text_vital='discrete')
+        transform_values_discrete_dict, data_discrete_dict = self._get_data_tuples(dist_transform=dist_transform, continuous=False)
+
+        self.print_info(text_normal='', text_vital='Starting fitting of distributions, in randomized order.')
+        return generate_parametric_fits(
+            ds=self.ds,
+            num_jobs=self.num_cpus,
+            fitter_type=self.use_fitter,
+            dist_transform=dist_transform,
+            selected_rvs_c=self.selected_rvs_c,
+            selected_rvs_d=self.selected_rvs_d,
+            data_dict=data_dict,
+            data_discrete_dict=data_discrete_dict,
+            transform_values_dict=transform_values_dict,
+            transform_values_discrete_dict=transform_values_discrete_dict)
+
+
 
     def fit_parametric(self) -> None:
         self.q.print('\n' + 10*'-')
@@ -116,14 +177,15 @@ This is so that this process can be interrupted and resumed.
         self.df = pd.read_csv(filepath_or_buffer=str(datafile), index_col=False)
         self.df[self.use_ds['colname_type']] = self.df[self.use_ds['colname_type']].astype(str)
         self.df[self.use_ds['colname_context']] = self.df[self.use_ds['colname_context']].astype(str)
+        self.ds = Dataset(ds=self.use_ds, df=self.df)
 
         
-        selected_rvs_c = self._select_continuous_rvs()
-        self.print_info(text_normal='Having selected ', text_vital=str(len(selected_rvs_c)), end='')
+        self.selected_rvs_c = list(self._select_continuous_rvs())
+        self.print_info(text_normal='Having selected ', text_vital=str(len(self.selected_rvs_c)), end='')
         self.q.print(' continuous random variables.')
 
-        selected_rvs_d = self._select_discrete_rvs()
-        self.print_info(text_normal='Having selected ', text_vital=str(len(selected_rvs_d)), end='')
+        self.selected_rvs_d = list(self._select_discrete_rvs())
+        self.print_info(text_normal='Having selected ', text_vital=str(len(self.selected_rvs_d)), end='')
         self.q.print(' discrete random variables.')
 
 
@@ -143,6 +205,27 @@ variable genetic algorithm problems, tailored to each random variable.
         ], prompt='Which fitter do you want to use?')
 
 
-        self._fit_continuous_rvs(rvs=selected_rvs_c)
+        self.q.print(10*'-')
+        self.q.print('''
+We are ready now to fit random variables. Between each of the transform types
+(expectation/mean, median, mode, supremum, infimum) you get the chance to pause
+and resume later, and the process will skip existing computed transforms. It is
+recommended to run the following on a resourceful computer and to exploit the
+highest possible degree of parallelism available. Next, you will be asked how
+many cores you would like to use.
+        '''.strip())
+        max_cpus = cpu_count()
+        self.num_cpus = int(self.q.text(f'How many cores should I use (1-{max_cpus})?', default=str(max_cpus), validate=lambda s: isint(s) and int(s) > 0 and int(s) <= max_cpus).ask())
 
-        pass
+        for dist_transform in list(DistTransform):
+            result_file = self.fits_dir.joinpath(f'./pregen_distns_{dist_transform.name}.pickle')
+            self.q.print(10*'-')
+
+            if result_file.exists():
+                self.print_info(text_normal='Fits already computed for transform: ', text_vital=f'{dist_transform.value} [{dist_transform.name}]')
+                continue
+            
+            self.print_info(text_normal='Generating parametric fits for: ', text_vital=f'{dist_transform.value} [{dist_transform.name}]')
+            res = self._fit_parametric(dist_transform=dist_transform)
+            with open(file=str(result_file), mode='wb') as fp:
+                dump(obj=res, file=fp)
