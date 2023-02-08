fd0419c615aa45d607866d57faf946b14fd237c1
Sebastian Hönel <development@hoenel.net>
2023-02-08 21:50:24 +0100

    Clean up and declare a Dataset as an additional export

--- src/metrics_as_scores/webapp/data.py
+++ src/metrics_as_scores/webapp/data.py
@@ -1,12 +1,7 @@
-from os import getcwd
-from os.path import join
-from sys import path
-from pathlib import Path
-path.append(f'{Path(join(getcwd(), "src")).resolve()}')
-
 from typing import Union
-from metrics_as_scores.distribution.distribution import Empirical, KDE_approx, Parametric
+from metrics_as_scores.distribution.distribution import Empirical, KDE_approx, Parametric, Dataset
 from metrics_as_scores.tools.lazy import SelfResetLazy
 
 
 cdfs: dict[str, SelfResetLazy[dict[str, Union[Empirical, KDE_approx, Parametric]]]] = None
+ds: Dataset = None
