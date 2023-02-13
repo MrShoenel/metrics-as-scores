e14df9eee57233ce7896ce5a147c470d0a3477e2
Sebastian Hönel <development@hoenel.net>
2023-02-13 23:17:01 +0100

    Allow package to determine if it runs local or as package and export some directories

--- src/metrics_as_scores/__init__.py
+++ src/metrics_as_scores/__init__.py
@@ -1,5 +1,10 @@
 from sys import path
 from pathlib import Path
-mas_dir = Path(__file__).resolve().parent
-path.append(str(mas_dir.resolve()))
-path.append(str(mas_dir.parent.resolve()))
+from metrics_as_scores.__version__ import IS_MAS_LOCAL
+MAS_DIR = Path(__file__).resolve().parent
+path.append(str(MAS_DIR.resolve()))
+path.append(str(MAS_DIR.parent.resolve()))
+
+DATASETS_DIR = (MAS_DIR.parent.parent if IS_MAS_LOCAL else MAS_DIR).joinpath('./datasets')
+if not DATASETS_DIR.exists():
+    DATASETS_DIR.mkdir(exist_ok=False)
--- src/metrics_as_scores/__version__.py
+++ src/metrics_as_scores/__version__.py
@@ -6,11 +6,18 @@ proj_dir = Path(__file__).parent.parent.parent
 pyproject_local = this_dir.joinpath('./pyproject.toml')
 pyproject_root = proj_dir.joinpath('./pyproject.toml')
 
+# If MAS is installed as PyPI package, this is False.
+IS_MAS_LOCAL: bool = False
 __version__: str = None
 if pyproject_root.exists() and load(pyproject_root)['tool']['poetry']['name'] == 'metrics-as-scores':
+    IS_MAS_LOCAL = True
     __version__ = load(pyproject_root)['tool']['poetry']['version']
 elif pyproject_local.exists():
     __version__ = load(pyproject_local)['tool']['poetry']['version']
 else:
     raise Exception('Cannot determine version.')
 
+del this_dir
+del proj_dir
+del pyproject_local
+del pyproject_root
